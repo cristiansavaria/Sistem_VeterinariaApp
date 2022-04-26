3c01bda58fd3208fdf29d29d62cbbc74d19d2133
cristiansavaria <cristian2012sa@gmail.com>
2022-04-26 00:39:27 -0400

    se configura la base de datos mysql

--- VeterinariaConfig/settings.py
+++ VeterinariaConfig/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'VeterinariaConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'veterinariaapp',
+        'USER': 'veterinaria',
+        'PASSWORD': 'jotes2007@',
+        'HOST': 'veterinaria.mysql.database.azure.com',
+        'PORT': '3306',
     }
 }
 
