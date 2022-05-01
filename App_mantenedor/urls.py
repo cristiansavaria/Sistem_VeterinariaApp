34e26b6b47e87cde581bad1e51fa940eb8ff5191
cristiansavaria <cristian2012sa@gmail.com>
2022-04-30 22:52:41 -0400

    se agregan cambios en panel admin

--- App_mantenedor/urls.py
+++ App_mantenedor/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path
-from .views import index
+from .views import clientes, index, insumos, pacientes, reserva_horas
 
 urlpatterns = [
     path('index/', index, name='index'),
+    path('clientes/', clientes, name='clientes'),
+    path('pacientes/', pacientes, name='pacientes'),
+    path('insumos/', insumos, name='insumos'),
+    path('reserva_horas/', reserva_horas, name='reserva_horas'),
     
    
    
--- App_mantenedor/views.py
+++ App_mantenedor/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def clientes(request):
+    return render(request, 'clientes.html')
+def pacientes(request):
+    return render(request, 'pacientes.html')
+def insumos(request):
+    return render(request, 'insumos.html')
+def reserva_horas(request):
+    return render(request, 'reserva_horas.html')
