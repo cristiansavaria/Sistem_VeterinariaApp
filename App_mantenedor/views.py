f4f6d1e6590fc215f6c8dc9578847d47117fc183
cristiansavaria <cristian2012sa@gmail.com>
2022-06-07 00:55:57 -0400

    .

--- App_mantenedor/views.py
+++ App_mantenedor/views.py
@@ -9,20 +9,19 @@ from django.http import Http404
 # Create your views here.
 
 
-
 def login(request):
     return render(request, 'registration/login.html')
-
 
 
 @login_required()
 def index(request):
     return render(request, 'index.html')
 
+
 @login_required()
 def clientes(request):
     cliente = Cliente.objects.all()
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
         paginator = Paginator(cliente, 7)
         cliente = paginator.page(page)
@@ -37,15 +36,16 @@ def clientes(request):
         formulario = ClienteForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
-            
-            
+
         data["form"] = formulario
 
     return render(request, 'clientes.html', data)
+
+
 @login_required()
 def pacientes(request):
     pacientes = Paciente.objects.all()
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
         paginator = Paginator(pacientes, 7)
         pacientes = paginator.page(page)
@@ -53,24 +53,23 @@ def pacientes(request):
         raise Http404
     data = {
         'entity': pacientes,
-        'form': PacienteForm(), 
+        'form': PacienteForm(),
         'paginator': paginator
     }
     if request.method == 'POST':
         formulario = PacienteForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
-            
-            
-            
-            
+
         data["form"] = formulario
 
     return render(request, 'pacientes.html', data)
+
+
 @login_required()
 def medico(request):
-    medico = Empleado.objects.filter(tipo_empleado_idtip_emp = 1)
-    page = request.GET.get('page',1)
+    medico = Empleado.objects.filter(tipo_empleado_idtip_emp=1)
+    page = request.GET.get('page', 1)
     try:
         paginator = Paginator(medico, 7)
         medico = paginator.page(page)
@@ -85,15 +84,16 @@ def medico(request):
         formulario = MedicoForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
-            
+
         data["form"] = formulario
-       
+
     return render(request, 'medico.html', data)
+
 
 @login_required()
 def insumos(request):
     insumos = Insumo.objects.all()
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
         paginator = Paginator(insumos, 7)
         insumos = paginator.page(page)
@@ -108,13 +108,10 @@ def insumos(request):
         formulario = InsumoForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
-                
+
         data["form"] = formulario
 
-
     return render(request, 'insumos.html', data)
-
-
 
 
 @login_required()
@@ -124,50 +121,56 @@ def reserva_horas(request):
 
 @login_required()
 def modificar_insumo(request, id_insumo):
-    
+
     insumo = get_object_or_404(Insumo, id_insumo=id_insumo)
     data = {
         'form': InsumoForm(instance=insumo)
     }
     if request.method == 'POST':
-        formulario = InsumoForm(data=request.POST, instance=insumo, files=request.FILES)
+        formulario = InsumoForm(
+            data=request.POST, instance=insumo, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
             return redirect(to="insumos")
         data["form"] = formulario
 
-
     return render(request, 'modificar_insumo.html', data)
+
+
 @login_required()
 def modificar_cliente(request, id_rut):
     cliente = get_object_or_404(Cliente, id_rut=id_rut)
     data = {
-        'form':ClienteForm(instance=cliente)
+        'form': ClienteForm(instance=cliente)
     }
     if request.method == 'POST':
-        formulario = ClienteForm(data=request.POST, instance=cliente, files=request.FILES)
+        formulario = ClienteForm(
+            data=request.POST, instance=cliente, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
             return redirect(to="clientes")
         data["form"] = formulario
 
-
     return render(request, 'modificar_cliente.html', data)
+
+
 @login_required()
 def modificar_medico(request, id_emp):
     medico = get_object_or_404(Empleado, id_emp=id_emp)
     data = {
-        'form':MedicoForm(instance=medico)
+        'form': MedicoForm(instance=medico)
     }
     if request.method == 'POST':
-        formulario = MedicoForm(data=request.POST, instance=medico, files=request.FILES)
+        formulario = MedicoForm(
+            data=request.POST, instance=medico, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
             return redirect(to="medico")
         data["form"] = formulario
 
-
     return render(request, 'modificar_medico.html', data)
+
+
 @login_required()
 def modificar_paciente(request, id_pac):
     paciente = get_object_or_404(Paciente, id_pac=id_pac)
@@ -175,17 +178,11 @@ def modificar_paciente(request, id_pac):
         'form': PacienteForm(instance=paciente)
     }
     if request.method == 'POST':
-        formulario = PacienteForm(data=request.POST, instance=paciente, files=request.FILES)
+        formulario = PacienteForm(
+            data=request.POST, instance=paciente, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
             return redirect(to="pacientes")
         data["form"] = formulario
 
-
     return render(request, 'modificar_paciente.html', data)
-
-
-
-
-
-
