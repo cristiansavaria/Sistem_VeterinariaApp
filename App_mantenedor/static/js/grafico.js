d04085f857dbbf9e68847247335dda44603a2bed
cristiansavaria <cristian2012sa@gmail.com>
2022-06-13 22:12:44 -0400

    full calendar

--- App_mantenedor/static/js/grafico.js
+++ App_mantenedor/static/js/grafico.js
@@ -1,6 +1,6 @@
 
 
-  console.log('hola')
+
 
 
   const labels = [
@@ -37,37 +37,4 @@
 
 
 
-  const data = {
-    labels: [
-      'Red',
-      'Green',
-      'Yellow',
-      'Grey',
-      'Blue'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [11, 16, 7, 3, 14],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(75, 192, 192)',
-        'rgb(255, 205, 86)',
-        'rgb(201, 203, 207)',
-        'rgb(54, 162, 235)'
-      ]
-    }]
-  };
-  // </block:setup>
-  
-  // <block:config:0>
-  const configs = {
-    type: 'polarArea',
-    data: data,
-    options: {}
-  };
-  // </block:config>
-  
-  const myCharts = new Chart(
-    document.getElementById('myCharts'),
-    configs
-  );
+
